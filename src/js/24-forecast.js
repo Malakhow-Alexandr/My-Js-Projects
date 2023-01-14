d1cae1d7170e8c8687ebc7324b0e9bbb28af2cfe
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-01-14 20:17:38 +0200

    add weather input

--- src/js/24-forecast.js
+++ src/js/24-forecast.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('.js-weather');
 const list = document.querySelector('.js-list');
-console.log(list);
 
 form.addEventListener('submit', onSearch);
 
@@ -11,9 +10,13 @@ function onSearch(evt) {
     query: { value: query },
     days: { value: days },
   } = evt.currentTarget.elements;
+  if(!query){return alert('Please add value!')}
 
-  
-  weatherAPI(query, days).then(data => {createMarkup(data)});
+  weatherAPI(query, days).then(data => {
+    list.innerHTML = createMarkup(data.forecast.forecastday);
+  }).catch(error => {
+    console.log(error);
+  });
 }
 
 function weatherAPI(query, days) {
@@ -21,30 +24,41 @@ function weatherAPI(query, days) {
   const API_KEY = 'f60c865d67554350816202559231101';
   return fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&days=${days}`)
     .then(response => {
-      if (!response) {
+      if (!response.ok) {
         throw new Error(response.statusText);
       }
       return response.json();
-    })
+    }).catch(error => {
+      console.log(error);
+    });
 }
 
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        day: {
+          condition: { icon, text },
+          avgtemp_c,
+        },
+        date,
+        astro: { sunrise, sunset },
+      }) =>
+        `<li class="js-list__item">
 
-// function createMarkup(arr) {
-//   return arr.map(({})) => {
-//     `<li class="js-list__item">
-
-// <img class="js-list__img" src="" alt="" />
-
-// <h2 class="js-list__img-desc"></h2>
-
-// <h3 class="js-list__forecast-data"></h3class>></h3>
-
-// <h4 class="js-list__temperature"></h4>
-
-// <h5 class="js-list__sunrise"></h5>
-
-// <h5 class="js-list__sunset"></h5>
-
-// </li>`;
-//   }).join('');
-// }
+  <img class="js-list__img" src="${icon}" alt="${text}" />
+  
+  <h2 class="js-list__img-desc">Img desc:${text}</h2>
+  
+  <h3 class="js-list__forecast-date">Date:${date}</h3>
+  
+  <h4 class="js-list__temperature">Average temperature:${avgtemp_c}</h4>
+  
+  <h5 class="js-list__sunrise"> Sunrise:${sunrise}</h5>
+  
+  <h5 class="js-list__sunset">Sunset:${sunset}</h5>
+  
+  </li>`
+    )
+    .join('');
+}
